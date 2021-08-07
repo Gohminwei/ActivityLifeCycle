dbaf698b54e09a6c57cefb97ad4ad71c29c162af
Goh Min Wei <gohmw-wm18@student.tarc.edu.my>
2021-08-07 12:18:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.activitylifecycle
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("myActivity", "OnCreate")
+
+        val btn : Button = findViewById(R.id.btnNext)
+
+        btn.setOnClickListener(){
+            var intent : Intent = Intent(this, SecondActivity::class.java )
+
+            startActivity(intent)
+        }
+    }
+}
